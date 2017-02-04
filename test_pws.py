5a6de65e63a5f640ca318d3a28ff4b421e226eba
theonlyphcs <sonpaulhenry@yahoo.com>
2017-02-04 21:22:36 +0800

    updates

--- test_pws.py
+++ test_pws.py
@@ -1,5 +1,4 @@
 #EnviMainProgram
-from i2clibraries import i2c_hmc5883l
 from sense_hat import SenseHat
 import time,math
 import datetime
@@ -26,12 +25,12 @@ def get_data():
 
 #function for logging to .csv format
 def csv_log():
-    
+    print("test")
 
 #start of main program
 while True:
      sense_data=get_data()
-     message = 'Temp in C* is {0} in F* is {1}  | Pressure is {2} mbars | Humidity is {3} percent | '.format(sense_data[0],sense_data[1],sense_data[2],sense_data[3])
+     message = 'Temp in C* is {0} in F* is {1}  | Pressure is {2} mbars | Humidity is {3} percent | \n'.format(sense_data[0],sense_data[1],sense_data[2],sense_data[3])
      print(message)
      time.sleep(30)
 
