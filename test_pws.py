8a31cb486660d3550bd9cb8d3c459ebe1352d56a
theonlyphcs <sonpaulhenry@yahoo.com>
2017-02-05 18:22:32 +0800

    working logging, raingauge, windspeed

--- test_pws.py
+++ test_pws.py
@@ -1,30 +1,42 @@
+#WORKING
+#!/usr/bin/python3
 
-#EnviMainProgram
 from sense_hat import SenseHat
-import time,math
-import datetime
-import csv
 import RPi.GPIO as GPIO
-import serial
+import time, math
 import os
-import spidev
+import time 
+from time import sleep
+from datetime import datetime
 
-wspeed=0
-rff=0
+wind_pin = 21
+rain_pin = 26
+wind_count = 0
+rain_count = 0
+rf = 0
 
-#function for getting data
-def get_data():     
-     sense_data =[]  
-     sense = SenseHat()
-     tempC = round(sense.get_temperature())
-     sense_data.append(tempC)
-     tempF = round(sense.get_temperature_from_pressure()*1.8 +32)
-     sense_data.append(tempF)
-     press = round(sense.get_pressure())
-     sense_data.append(press)
-     humid = round(sense.get_humidity())
-     sense_data.append(humid)
-     return sense_data
+def bucket_tipped(rf):
+    global rain_count
+    rf=rain_count * 0.2794
+    return rf
+
+def calculate_speed(r_cm, time_sec):
+    global wind_count
+    circ_cm = (2 * math.pi) * r_cm
+    rot = wind_count / 2.0
+    dist_km = (circ_cm * rot) / 100000.0 # convert to kilometres
+    km_per_sec = dist_km / time_sec
+    km_per_hour = km_per_sec * 3600 # convert to distance per hour
+    return km_per_hour * 1.18
+
+def spin(channel):
+    global wind_count
+    wind_count += 1
+   
+
+def tip(channel2):
+    global rain_count
+    rain_count += 1
 
 #function for logging to excel file
 def csv_log():
@@ -34,19 +46,55 @@ def csv_log():
             file.write("Time,TempCelsius,TempFarenheit,Pressure,Humidity,Windspeed,Rainfall\n")
     i=i+1
     now = datetime.now()
-    file.write(str(now)+","+ str(tempC) +","+ str(tempF) +","+ str(press) +","+ str(humid)+","+ str(wspeed) +","+ str(rff) +"\n")
+    file.write(str(now)+","+ str(sense_data[0]) +","+ str(sense_data[1]) +","+ str(sense_data[2]) +","+ str(sense_data[3])+","+ str(wspeed) +","+ str(rff) +"\n")
     file.flush()
     file.close()
     print("success logging \n")
-    
-#start of main program
-while True:
-     sense_data=get_data()
-     message = 'Temp in C* is {0} in F* is {1}  | Pressure is {2} mbars | Humidity is {3} percent | \n'.format(sense_data[0],sense_data[1],sense_data[2],sense_data[3])
-     print(message)
-     time.sleep(30)
 
-     
+#function for getting data
+def get_data():
+    
+    sense_data =[]  
+    sense = SenseHat()
+    tempC = round(sense.get_temperature())
+    sense_data.append(tempC)
+    tempF = round(sense.get_temperature_from_pressure()*1.8 +32)
+    sense_data.append(tempF)
+    press = round(sense.get_pressure())
+    sense_data.append(press)
+    humid = round(sense.get_humidity())
+    sense_data.append(humid)
+    return sense_data
+    
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(wind_pin, GPIO.IN, GPIO.PUD_UP)
+GPIO.setup(rain_pin, GPIO.IN, GPIO.PUD_UP)
+GPIO.add_event_detect(wind_pin, GPIO.FALLING, callback=spin)
+GPIO.add_event_detect(rain_pin, GPIO.FALLING, callback=tip, bouncetime=300)
+interval = 5
+
+while True:
+    wind_count = 0
+    rain_count = 0
+    time.sleep(interval)
+    wspeed = calculate_speed(9.0, interval)
+    rff = bucket_tipped(rf)
+    #print ("Windspeed: ",wspeed, "kph" ,wind_count)
+    #print ("Rainfall: ",rff, "mm", rain_count, '\n')
+    sense_data=get_data()
+    csv_log()
+    message = 'Temp in C* is {0} in F* is {1}  | Pressure is {2} mbars | Humidity is {3} percent | WindSpeed is {4} kph | Rainfall is {5} mm | \n'.format(sense_data[0],sense_data[1],sense_data[2],sense_data[3], wspeed, rff)
+    print(message,'\n')
+    time.sleep(10)
+    
+
+#WORKING
+
+
+
+
+
+
      
 
 
