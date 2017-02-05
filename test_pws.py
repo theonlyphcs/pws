3902ba1f8e93b40281d6ee9c73d0c25cdb743cc7
theonlyphcs <sonpaulhenry@yahoo.com>
2017-02-05 09:16:21 +0800

    added windspeed and rainfall 1.4

--- test_pws.py
+++ test_pws.py
@@ -1,5 +1,5 @@
+#MainProgram
 
-#EnviMainProgram
 from sense_hat import SenseHat
 from time import sleep
 from datetime import datetime
@@ -12,8 +12,13 @@ import spidev
 
 wspeed=0
 rff=0
+wind_pin = 21
+rain_pin = 26
+wind_count = 0
+rain_count = 0
+interval =5
 
-#function for getting data
+##### function for getting data from senseHAT #####
 def get_data():     
      sense_data =[]  
      sense = SenseHat()
@@ -27,7 +32,33 @@ def get_data():
      sense_data.append(humid)
      return sense_data
 
-#function for logging to excel file
+##### function for rainfall computation
+def bucket_tipped(rf):
+    global rain_count
+    rf=rain_count * 0.2794 #0.2794 Milimeters per bucket
+    return rf
+
+##### function for windspeed computation
+def calculate_speed(r_cm, time_sec):
+    global wind_count
+    circ_cm = (2 * math.pi) * r_cm
+    rot = wind_count / 2.0
+    dist_km = (circ_cm * rot) / 100000.0 # convert to kilometres
+    km_per_sec = dist_km / time_sec
+    km_per_hour = km_per_sec * 3600 # convert to distance per hour
+    return km_per_hour * 1.18
+
+##### additional function for windspeed computation 
+def spin(channel):
+    global wind_count
+    wind_count += 1
+
+##### additional function for windspeed computation 
+def tip(channel2):
+    global rain_count
+    rain_count += 1
+
+###### function for logging to excel file
 def csv_log():
     file = open("/media/pi/SONDISK/data_log.csv", "a")
     i=0
@@ -39,12 +70,14 @@ def csv_log():
     file.flush()
     file.close()
     print("...success logging \n")
-    
-#start of main program
+
+###### start of main program #####
 while True:
      sense_data=get_data()
      message = 'Temp in C* is {0} in F* is {1}  | Pressure is {2} mbars | Humidity is {3} percent | \n'.format(sense_data[0],sense_data[1],sense_data[2],sense_data[3])
      csv_log()
+     print ("Windspeed: ",calculate_speed(9.0, interval), "kph" ,wind_count)
+     print ("Rainfall: ",bucket_tipped(rf), "mm", rain_count)
      print(message)
      time.sleep(30)
 
